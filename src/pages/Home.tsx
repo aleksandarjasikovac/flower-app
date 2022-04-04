3aa4bc50e21c369c4cd3e024a1dc994aa41a86e4
Aleksandar J <74408889+alex-aestic@users.noreply.github.com>
2022-04-04 13:38:07 +0200

    cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,4 @@
-import React, { FC, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "../hooks/useTypedSelector";
-import { postActions } from "../store";
+import React, { FC } from "react";
 
 import { Hero } from "../components/Hero";
 import { Layout } from "../components/Layout";
